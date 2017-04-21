0e7b7672194858d6877eda64bfd2315f669b4065
Heein Park <hpark24@pratt.edu>
2017-04-21 16:41:41 +0000

    USPS

--- product-page/js/main.js
+++ product-page/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $("header form").submit(
+    $(".success-response p").hide();
+    
+    $(".email-form").each(function (){
+        var selectedForm = $(this);
+        
+        console.log(selectedForm);
+        selectedForm.submit(
             function(event) {
                 var formData = $(this).serialize();
                 
@@ -11,15 +17,21 @@ $(document).ready(function() {
                     dataType: "json"
                 }).done(function(data) {
                     console.log(data.email);
-                    $("header form input[type='text']").val("");
+                    selectedForm.children("form input[type='text']").val("");
+                    selectedForm.parent().children(".success-response").children("p").hide();
+                    selectedForm.parent().children(".success-response").children("p").text("We receive your address. Thank you!").removeClass("error-red").show(300).delay(3200).hide(300);
+                    // selectedForm.parent().children(".success-response").children("p").delay(3200).hide(300);
                 }).fail(function(data) {
-                    alert(JSON.parse(data.responseText).email[0]);
+                    // alert(JSON.parse(data.responseText).email[0]);
+                    var errorMsg = JSON.parse(data.responseText).email[0];
+                    selectedForm.parent().children(".success-response").children("p").addClass("error-red").text(errorMsg).show();
                 });
                 
                 event.preventDefault();
             });
+    });
             
-    $("#ups-form form").submit(
+    $("#usps-form form").submit(
             function(event) {
                 var formData = $(this).serializeArray();
                 
@@ -27,25 +39,37 @@ $(document).ready(function() {
                 
                 console.log(formData);
                 
-                event.preventDefault();
-            });
-            
-    $("footer form").submit(
-            function(event) {
-                var formData = $(this).serialize();
-                
                 $.ajax({
-                    type: "POST",
-                    url: "https://web2-product-page.herokuapp.com/subscribers",
+                    type: "GET",
+                    url: 'https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=<TrackRequest USERID="849PRATT1002"><TrackID ID="9400111899223130473898"></TrackID></TrackRequest>',
                     data: formData,
-                    dataType: "json"
+                    dataType: "xml"
                 }).done(function(data) {
-                    console.log(data.email);
-                    $("header form input[type='text']").val("");
+                    console.log(data);
                 }).fail(function(data) {
-                    alert(JSON.parse(data.responseText).email[0]);
                 });
                 
                 event.preventDefault();
             });
+            
+    // $("footer form").submit(
+    //         function(event) {
+    //             var formData = $(this).serialize();
+                
+    //             $.ajax({
+    //                 type: "POST",
+    //                 url: "https://web2-product-page.herokuapp.com/subscribers",
+    //                 data: formData,
+    //                 dataType: "json"
+    //             }).done(function(data) {
+    //                 console.log(data.email);
+    //                 $("footer form input[type='text']").val("");
+    //                 $("#response-footer > h4").show(300).text("We receive your address. Thank you!");
+    //                 $("#response-footer > h4").delay(3200).hide(300);
+    //             }).fail(function(data) {
+    //                 alert(JSON.parse(data.responseText).email[0]);
+    //             });
+                
+    //             event.preventDefault();
+    //         });
 });
